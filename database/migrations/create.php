95743ce3e6ac50af27f76a60fd6d8328ba642a94
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-12-24 20:02:31 +0100

    update create and drop scripts

--- database/migrations/create.php
+++ database/migrations/create.php
@@ -1,51 +1,92 @@
 <?php
 
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
+$database = 'library_system';
 
-function createTables()
+function createTables($host, $username, $password, $database)
 {
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDBPDO";
-
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $users = "CREATE TABLE users (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            first_name VARCHAR(255) NOT NULL,
-            last_name VARCHAR(255) NOT NULL,
-            email VARCHAR(255),
-            password VARCHAR(255) NOT NULL,
-            membership BOOLEAN NOT NULL,
-            admin BOOLEAN NOT NULL,
-        )";
+        $queries = [
+            "CREATE TABLE users (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                first_name VARCHAR(255) NOT NULL,
+                last_name VARCHAR(255) NOT NULL,
+                email VARCHAR(255),
+                password VARCHAR(255) NOT NULL,
+                membership BOOLEAN NOT NULL,
+                admin BOOLEAN NOT NULL
+            )",
+            "CREATE TABLE books (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                author VARCHAR(255) NOT NULL,
+                isbn VARCHAR(255),
+                available BOOLEAN NOT NULL,
+                available_on DATE
+            )",
+            "CREATE TABLE book_loans (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                user_id INT UNSIGNED NOT NULL,
+                book_id INT UNSIGNED NOT NULL,
+                extended BOOLEAN NOT NULL,
+                return_date DATE,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE ON UPDATE CASCADE
+            )"
+        ];
 
-        $books = "CREATE TABLE books (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            author VARCHAR(255) NOT NULL,
-            isbn VARCHAR(255),
-            available BOOLEAN NOT NULL,
-            available_on DATE,
-        )";
+        foreach ($queries as $query) {
+            $conn->exec($query);
+        }
+        echo "Tables created successfully\n";
 
-        $book_loans = "CREATE TABLE books (
-            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL FOREIGN KEY,
-            book_id VARCHAR(255) NOT NULL FOREIGN KEY,
-            extended BOOLEAN NOT NULL,
-            return_date DATE,
-        )";
+        // generate sample data
+        $sampleData = [
+            "INSERT INTO users (first_name, last_name, email, password, membership, admin) VALUES
+                ('John', 'Doe', 'john.doe@example.com', 'password', 1, 0),
+                ('Jane', 'Doe', 'jane.doe@example.com', 'password', 1, 0),
+                ('admin', 'admin', 'admin@example.com', 'password', 1, 1)",
+            "INSERT INTO books (title, author, isbn, available, available_on) VALUES
+                ('Book 1', 'Author 1', '1234567890', 1, NULL),
+                ('Book 2', 'Author 2', '0987654321', 1, NULL),
+                ('Book 3', 'Author 3', '1231231230', 0, '2022-01-01')",
+            "INSERT INTO book_loans (user_id, book_id, extended, return_date) VALUES
+                (1, 1, 0, '2022-01-01'),
+                (2, 2, 1, '2022-02-01')"
+        ];
 
-        // use exec() because no results are returned
-        $conn->exec($users, $books, $book_loans);
-        echo "Tables created successfully";
+        foreach ($sampleData as $query) {
+            $conn->exec($query);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function createDatabase($host, $username, $password, $database)
+{
+    try {
+        $conn = new PDO("mysql:host=$host", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // create the database
+        $sql = "CREATE DATABASE IF NOT EXISTS $database";
+        $conn->exec($sql);
+        echo "Database created successfully\n";
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
 
     $conn = null;
 }
+
+// example usage
+createDatabase($host, $username, $password, $database);
+createTables($host, $username, $password, $database);
--- database/migrations/drop.php
+++ database/migrations/drop.php
@@ -1,24 +1,26 @@
 <?php
-function dropDb()
-{
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDBPDO";
 
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
+$database = 'library_system';
+
+function dropDatabase($host, $username, $password, $database)
+{
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$host", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = 'DROP DATABASE library_system';
-
-        // use exec() because no results are returned
+        $sql = "DROP DATABASE IF EXISTS $database";
         $conn->exec($sql);
-        echo "Database dropped successfully";
+        echo "Database dropped successfully\n";
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
 
     $conn = null;
 }
+
+// example usage
+dropDatabase($host, $username, $password, $database);
