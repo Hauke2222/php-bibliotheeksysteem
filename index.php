a8fc4561feb8f3114c5f8fa27e9982ea3362e716
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-12-30 12:11:54 +0100

    begin new router

--- index.php
+++ index.php
@@ -8,9 +8,8 @@ use LibrarySystem\Controllers\AuthController;
 use LibrarySystem\Controllers\BookController;
 use LibrarySystem\Controllers\UserController;
 
-require 'controllers/AuthController.php';
-require 'controllers/BookController.php';
-require 'controllers/UserController.php';
+
+require 'router.php';
 
 try {
     $pdo = new PDO('mysql:host=127.0.0.1;dbname=library_system', 'root', '');
@@ -18,46 +17,46 @@ try {
     die('Could not connect to the database.');
 }
 
-$request_uri = $_SERVER['REQUEST_URI'];
+// $request_uri = $_SERVER['REQUEST_URI'];
 
-switch ($request_uri) {
-    case '/':
-        // code to handle the homepage
-        break;
-    case '/books':
-        $controller = new BookController($pdo);
-        $books = $controller->getAll();
-        require 'views/books/books.view.php';
-        break;
-    case '/books/loans':
-        $userController = new UserController($pdo);
-        $bookController = new BookController($pdo);
-        $loans = $bookController->getAllLoans();
-        require 'views/books/loans.view.php';
-        break;
-    case '/books/create':
-        $authController = new AuthController($pdo);
-        if ($authController->isAdmin()) {
-            $controller = new BookController($pdo);
-            require 'views/books/create.view.php';
-        } else {
-            // User is not authenticated, redirect to login page
-            header('Location: /login');
-        }
-        break;
-    case '/users':
-        $controller = new UserController($pdo);
-        $users = $controller->getAll();
-        require 'views/users/users.view.php';
-        break;
-    case '/users/create':
-        $controller = new UserController($pdo);
-        require 'views/users/create.view.php';
-        break;
-    case '/login':
-        require 'views/login.view.php';
-    default:
-        // code to handle any other pages
-        http_response_code(404);
-        break;
-}
+// switch ($request_uri) {
+//     case '/':
+//         // code to handle the homepage
+//         break;
+//     case '/books':
+//         $controller = new BookController($pdo);
+//         $books = $controller->getAll();
+//         require 'views/books/books.view.php';
+//         break;
+//     case '/books/loans':
+//         $userController = new UserController($pdo);
+//         $bookController = new BookController($pdo);
+//         $loans = $bookController->getAllLoans();
+//         require 'views/books/loans.view.php';
+//         break;
+//     case '/books/create':
+//         $authController = new AuthController($pdo);
+//         if ($authController->isAdmin()) {
+//             $controller = new BookController($pdo);
+//             require 'views/books/create.view.php';
+//         } else {
+//             // User is not authenticated, redirect to login page
+//             header('Location: /login');
+//         }
+//         break;
+//     case '/users':
+//         $controller = new UserController($pdo);
+//         $users = $controller->getAll();
+//         require 'views/users/users.view.php';
+//         break;
+//     case '/users/create':
+//         $controller = new UserController($pdo);
+//         require 'views/users/create.view.php';
+//         break;
+//     case '/login':
+//         require 'views/login.view.php';
+//     default:
+//         // code to handle any other pages
+//         http_response_code(404);
+//         break;
+// }
--- router.php
+++ router.php
@@ -1,9 +1,47 @@
 <?php
 
-function routeToController($uri, $routes)
+use LibrarySystem\Controllers\AuthController;
+use LibrarySystem\Controllers\BookController;
+use LibrarySystem\Controllers\UserController;
+
+require 'controllers/AuthController.php';
+require 'controllers/BookController.php';
+require 'controllers/UserController.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/books' => ['controller' => 'BookController', 'method' => 'index'],
+    '/books/loans' => ['controller' => 'BookController', 'method' => 'loans'],
+    // '/books/create' => [
+    //     'controller' => 'BookController',
+    //     'method' => 'create',
+    //     'middleware' => 'IsAdmin',
+    // ],
+    '/users' => ['controller' => 'UserController', 'method' => 'index'],
+    '/users/create' => ['controller' => 'UserController', 'method' => 'create'],
+    '/login' => ['controller' => 'AuthController', 'method' => 'login'],
+];
+
+function routeToControllerMethod($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        $route = $routes[$uri];
+        $controllerName = $route['controller'];
+        $methodName = $route['method'];
+
+        // Check if the middleware exists and run it
+        // if (isset($route['middleware'])) {
+        //     $middlewareName = $route['middleware'];
+        //     $middleware = new $middlewareName();
+        //     if (!$middleware->handle()) {
+        //         return;
+        //     }
+        // }
+
+        // Create an instance of the controller and call the method
+        $controller = new $controllerName();
+        $controller->$methodName();
     } else {
         abort();
     }
@@ -12,13 +50,8 @@ function routeToController($uri, $routes)
 function abort($code = 404)
 {
     http_response_code($code);
-
-    require "views/{$code}.php";
-
+    echo "404 Not Found.";
     die();
 }
 
-$routes = require('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-routeToController($uri, $routes);
+routeToControllerMethod($uri, $routes);
